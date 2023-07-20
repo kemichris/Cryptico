17883793d8135556b0ef35b96c08ec2009877acc
Kemi Chris <ekeminichristopher123@gmail.com>
2023-07-20 03:40:46 +0100

    nav menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
+/* ////// NAV BAR ////// */
+const mobileNav = document.querySelector(".mobile-menu");
+const NavIcon = document.querySelector(".nav-menu-icon");
+const removeNav = document.querySelector(".remove-nav-icon");
+
+
+NavIcon.addEventListener("click", ()=> {
+    mobileNav.classList.remove("inactive");
+    NavIcon.classList.add("inactive")
+    removeNav.classList.remove("inactive")
+});
+
+removeNav.addEventListener("click", ()=> {
+    mobileNav.classList.add("inactive");
+    removeNav.classList.add("inactive")
+    NavIcon.classList.remove("inactive")
+});
+
+
+
+
+
+
+
+
 /* ////// TESTIMONY SECTION ////// */
 const testimonyContainer = document.querySelector(".testimony-container");
 const testimonys = document.querySelectorAll(".testimony");
