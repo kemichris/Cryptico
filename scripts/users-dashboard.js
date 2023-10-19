3badea84caf73ee98f717e965176c0eb957ec9d0
Kemi Chris <ekeminichristopher123@gmail.com>
2023-10-19 03:28:18 +0100

    Dashboard Responsiveness

--- scripts/users-dashboard.js
+++ scripts/users-dashboard.js
@@ -1,4 +1,4 @@
-/* ////// SIDE BAR ////// */
+/* ////// SIDE BAR VARs ////// */
 const account = document.querySelector(".account");
 const depWith = document.querySelector(".dep-with");
 const packages = document.querySelector(".packages");
@@ -7,6 +7,26 @@ const accountDropdown = document.querySelector(".account-dropdown");
 const depWithDropdown = document.querySelector(".dep-with-dropdown");
 const packagesDropdown = document.querySelector(".packages-dropdown");
 
+/* ////// NAV BAR ////// */
+const sideBar = document.querySelector(".user-side-bar");
+const NavIcon = document.querySelector(".nav-menu-icon");
+const removeNav = document.querySelector(".remove-nav-icon");
+
+NavIcon.addEventListener("click", ()=> {
+    sideBar.classList.remove("active");
+    NavIcon.classList.add("inactive")
+    removeNav.classList.remove("inactive")
+});
+
+removeNav.addEventListener("click", ()=> {
+    sideBar.classList.add("active");
+    removeNav.classList.add("inactive")
+    NavIcon.classList.remove("inactive")
+});
+
+
+
+// side bar events 
 account.addEventListener("click", ()=> {
     accountDropdown.classList.toggle("inactive")
 });
