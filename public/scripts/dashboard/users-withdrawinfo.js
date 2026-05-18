2567b6391716c7e0b9e8379a9ae9f3d5f4cf95b6
kemi Chris <ekeminichristopher123@gmail.com>
2026-05-18 10:34:39 +0100

    href correction

--- public/scripts/dashboard/users-withdrawinfo.js
+++ public/scripts/dashboard/users-withdrawinfo.js
@@ -1,13 +1,3 @@
-
-/* ////// SIDE BAR VARs ////// */
-const account = document.querySelector(".account");
-const depWith = document.querySelector(".dep-with");
-const packages = document.querySelector(".packages");
-
-const accountDropdown = document.querySelector(".account-dropdown");
-const depWithDropdown = document.querySelector(".dep-with-dropdown");
-const packagesDropdown = document.querySelector(".packages-dropdown");
-
 /* ////// form variables ////// */
 const openWithdrawInfo = document.querySelectorAll(".open-info-dropdown");
 const closeWithdrawInfo = document.querySelectorAll(".close-info-dropdown");
@@ -17,36 +7,6 @@ const ethForm = document.querySelector(".eth-form")
 
 
 
-
-/* ////// NAV BAR ////// */
-const sideBar = document.querySelector(".user-side-bar");
-const NavIcon = document.querySelector(".nav-menu-icon");
-const removeNav = document.querySelector(".remove-nav-icon");
-
-NavIcon.addEventListener("click", ()=> {
-    sideBar.classList.remove("active");
-    NavIcon.classList.add("inactive")
-    removeNav.classList.remove("inactive")
-});
-
-removeNav.addEventListener("click", ()=> {
-    sideBar.classList.add("active");
-    removeNav.classList.add("inactive")
-    NavIcon.classList.remove("inactive")
-});
-
-
-
-// side bar events 
-account.addEventListener("click", ()=> {
-    accountDropdown.classList.toggle("inactive")
-});
-depWith.addEventListener("click", ()=> {
-    depWithDropdown.classList.toggle("inactive")
-});
-packages.addEventListener("click", ()=> {
-    packagesDropdown.classList.toggle("inactive")
-});
 
 
 
