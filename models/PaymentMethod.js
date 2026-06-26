42aa591be09079b3411b107b4444bf82d0ea6ea1
kemi Chris <ekeminichristopher123@gmail.com>
2026-06-26 15:22:24 +0100

    payment setting html and css

--- models/PaymentMethod.js
+++ models/PaymentMethod.js
@@ -1,67 +1,73 @@
 const mongoose = require("mongoose");
 
-const paymentMethodSchema = new mongoose.Schema( {
-        // BTC, ETH, Bank Transfer, USDT TRC20...
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
+const paymentMethodSchema = new mongoose.Schema({
+    // BTC, ETH, Bank Transfer, USDT TRC20...
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
 
-        // bitcoin, ethereum, bank...
-        network: {
-            type: String,
-            required: true,
-        },
+    // bitcoin, ethereum, bank...
+    network: {
+        type: String,
+        required: true,
+    },
 
-        // crypto | bank
-        type: {
-            type: String,
-            enum: ["crypto", "currency"],
-            required: true
-        },
+    // crypto | bank
+    type: {
+        type: String,
+        enum: ["crypto", "currency"],
+        required: true
+    },
 
-        // Wallet address or bank account number
-        paymentAddress: {
-            type: String,
-            required: true,
-            trim: true
-        },
+    // Wallet address or bank account number
+    paymentAddress: {
+        type: String,
+        required: true,
+        trim: true
+    },
 
-        // Optional account holder
-        accountName: {
-            type: String,
-            default: ""
-        },
+    // Optional account holder
+    accountName: {
+        type: String,
+        default: ""
+    },
 
-        // Optional bank name
-        bankName: {
-            type: String,
-            default: ""
-        },
+    // Optional bank name
+    bankName: {
+        type: String,
+        default: ""
+    },
 
-        // Used for displaying logo
-        icon: {
-            type: String,
-            default: ""
-        },
+    // Used for displaying logo
+    icon: {
+        type: String,
+        default: ""
+    },
 
-        qrCode: {
-            type: String,
-        },
+    qrCode: {
+        type: String,
+    },
 
-        // Whether users can currently use it
-        status: {
-            type: String,
-            enum: ['enabled', 'disabled'],
-            default: 'enabled'
-        },
+    availableFor: {
+        type: String,
+        enum: ["deposit", "withdrawal", "both"],
+        default: "both"
+    },
 
-        instructions: {
-            type: String,
-            default: ""
-        },
-    }, { timestamps: true }
+    // Whether users can currently use it
+    status: {
+        type: String,
+        enum: ['enabled', 'disabled'],
+        default: 'enabled'
+    },
+
+    instructions: {
+        type: String,
+        default: ""
+    },
+}, { timestamps: true }
 );
 
 module.exports = mongoose.model("PaymentMethod", paymentMethodSchema);
