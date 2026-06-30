2a12e5c15fd2620ebf92441c143bdd83e65be3a1
kemi Chris <ekeminichristopher123@gmail.com>
2026-06-30 23:13:01 +0100

    api url update

--- cryptico-client/scripts/utils/config.js
+++ cryptico-client/scripts/utils/config.js
@@ -1,4 +1,4 @@
 const API_URL = window.location.hostname === "localhost" ? "http://localhost:3000/api" 
-: "https://cryptico-api.onrender.com/";
+: "https://cryptico-api.onrender.com";
 
 window.API_URL = API_URL;
