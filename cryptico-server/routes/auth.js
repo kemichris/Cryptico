ef151dec68488246d04b8dfb5441f0bb2b7e23e3
kemi Chris <ekeminichristopher123@gmail.com>
2026-06-30 23:37:48 +0100

    route

--- cryptico-server/routes/auth.js
+++ cryptico-server/routes/auth.js
@@ -6,6 +6,6 @@ router.post('/register', register);
 router.post('/register-admin', registerAdmin);
 router.post('/login', login);
 router.post("/verify-email", verifyEmail);
-router.post("/auth/resend-verification", resendVerificationCode);
+router.post("/resend-verification", resendVerificationCode);
 
 module.exports = router;
