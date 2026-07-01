9a332d1b785c15e887da871187297de9f53d56bc
kemi Chris <ekeminichristopher123@gmail.com>
2026-07-01 13:50:08 +0100

    update email service

--- cryptico-server/utils/mailer.js
+++ cryptico-server/utils/mailer.js
@@ -1,25 +1,8 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // STARTTLS
-  requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-(async () => {
-  try {
-    await transporter.verify();
-    console.log("✅ SMTP server is ready");
-  } catch (error) {
-    console.error("❌ SMTP verification failed:", error);
-  }
-})();
 // const transporter = nodemailer.createTransport({
 //     host: process.env.SMTP_HOST,
 //     port: process.env.SMTP_PORT,
@@ -31,29 +14,50 @@ const transporter = nodemailer.createTransport({
 // });
 
 const sendVerificationEmail = async (email, name, code) => {
-  await transporter.sendMail({
-    from: `"Cryptico" <${process.env.EMAIL_USER}>`,
-    to: email,
-    subject: "Email Verification Code",
-    html: `
-      <div style="font-family: Arial;">
-        <h2>Hello ${name}</h2>
-        <p>Your verification code is:</p>
-        <h1 style="color:#1e88e5">${code}</h1>
-        <p>This code expires in 10 minutes.</p>
-      </div>
-    `,
-  });
+  try {
+    await resend.emails.send({
+      from: "Cryptico <onboarding@resend.dev>",
+      to: email,
+      subject: "Email Verification Code",
+      html: `
+        <div style="font-family: Arial;">
+          <h2>Hello ${name}</h2>
+
+          <p>Your verification code is:</p>
+
+          <h1 style="color:#1e88e5">${code}</h1>
+
+          <p>This code expires in 10 minutes.</p>
+        </div>
+      `,
+    });
+
+    console.log("✅ Verification email sent");
+  } catch (error) {
+    console.error("❌ Email failed:", error);
+    throw error;
+  }
 };
 
 const sendMail = async ({ to, subject, html }) => {
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        html
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Cryptico <onboarding@resend.dev>",
+      to,
+      subject,
+      html,
     });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    return data;
+  } catch (err) {
+    console.error("❌ Email failed:", err);
+    throw err;
+  }
 };
+
 
 
 module.exports = { sendVerificationEmail, sendMail };
